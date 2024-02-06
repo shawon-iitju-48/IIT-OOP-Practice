1e1c2383d3431e15982fa7e9d1eef205002b8ad9
shawon-iitju-48 <shawon.iitju.48@gmail.com>
2024-02-07 01:06:44 +0600

    practice

---
+++ src/oop/practice/tutorial_02/CircleClass.java
@@ -0,0 +1,17 @@
+package oop.practice.tutorial_02;
+
+public class CircleClass {
+	
+	private int radius;
+	
+	public void setRadius(int r) {
+		this.radius=r;
+	}
+	
+	
+	public double getArea() {
+		return 3.14159*radius*radius;
+	}
+	
+	
+}
---
+++ src/oop/practice/tutorial_02/Driver.java
@@ -0,0 +1,13 @@
+package oop.practice.tutorial_02;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		CircleClass circle = new CircleClass();
+		circle.setRadius(10);
+		System.out.println(circle.getArea());
+		
+	}
+
+}
